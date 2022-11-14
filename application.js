a98307febd68195854078ecb4f638b87aecee9b8
akosgarai <szlem01@gmail.com>
2022-11-14 11:55:57 +0100

    Text rendering improvements

--- application.js
+++ application.js
@@ -6,12 +6,30 @@ class Application {
 		this.initCanvasVariables();
 	}
 	initCanvasVariables() {
-		this.dashLen = 220;
-		this.dashOffset = 220;
+		this.dashLen = 100;
+		this.dashOffset = 100;
 		this.speed = 5;
 		this.dirtyWord = '';
 		this.index = 0;
-		this.x = 30;
+		this.x = 10;
+		this.y = 0;
+		const canvas = document.getElementById('canvas');
+		const fontSizeRatio = 50 / 1845;
+		this.ctx = canvas.getContext('2d');
+		this.ctx.font = this.getFont();
+		this.ctx.lineWidth = 4;
+		this.ctx.lineJoin = "round";
+		this.ctx.globalAlpha = 2/3;
+		this.ctx.strokeStyle = "#ffffff";
+		this.ctx.fillStyle = "#ffffff";
+		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+	// calculate the font size based on the canvas size
+	// and return the string to be used for the canvas.font property
+	getFont() {
+		const canvas = document.getElementById('canvas');
+		const fontSizeRatio = 90 / 1000;
+		return '50px Comic Sans MS, cursive, TSCu_Comic, sans-serif';
 	}
     // Download the dirty words xml, parse it to JSON and store it in a variable.
 	doDownload() {
@@ -40,39 +58,32 @@ class Application {
 	displayNewDirty() {
 		this.initCanvasVariables();
 		this.dirtyWord = this.getDirty();
-		this.draw();
-	}
-	// draw the dirty word on the canvas
-	// The background of the canvas has to be black, the font color white and the font size 30px
-	draw() {
-		const canvas = document.getElementById('canvas');
-		this.ctx = canvas.getContext('2d');
-		this.ctx.font = "50px Comic Sans MS, cursive, TSCu_Comic, sans-serif"; 
-		this.ctx.lineWidth = 4;
-		this.ctx.lineJoin = "round";
-		this.ctx.globalAlpha = 2/3;
-		this.ctx.strokeStyle = "#ffffff";
-		this.ctx.fillStyle = "#ffffff";
-		this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+		//this.dirtyWord = 'árvíztűrő tükörfúrógép';
 		this.render();
 	}
 	render() {
-		this.ctx.clearRect(this.x, 0, 60, 150);
-		this.ctx.setLineDash([this.dashLen - this.dashOffset, this.dashOffset - this.speed]); // create a long dash mask
-		this.dashOffset -= this.speed;                                         // reduce dash length
-		this.ctx.strokeText(this.dirtyWord[this.index], this.x, 90);                               // stroke letter
-
-		if (this.dashOffset > 0) {
-			requestAnimationFrame(this.render.bind(this));             // animate
-		} else {
-			this.ctx.fillText(this.dirtyWord[this.index], this.x, 90);                               // fill final letter
+		//this.ctx.font = this.getFont();
+		if (this.dashOffset <= 0) {
+			this.ctx.fillText(this.dirtyWord[this.index], this.x, this.y + 50);  // fill final letter
 			this.dashOffset = this.dashLen;                                      // prep next char
-			this.x += this.ctx.measureText(this.dirtyWord[this.index++]).width + this.ctx.lineWidth * Math.random();
-			this.ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random());        // random y-delta
-			this.ctx.rotate(Math.random() * 0.005);                         // random rotation
-			if (this.index < this.dirtyWord.length) {
-				requestAnimationFrame(this.render.bind(this));
+			const nextCharSize = this.ctx.measureText(this.dirtyWord[this.index++]).width + this.ctx.lineWidth * Math.random();
+			// if the next size is bigger than the canvas width, start a new line
+			if (this.x + nextCharSize > this.ctx.canvas.width - 30) {
+				this.x = 10;
+				this.y += 60;
+			} else {
+				this.x += nextCharSize;
 			}
+			this.ctx.setTransform(1, 0, 0, 1, 0, 3 * Math.random()); // random y-delta
+			this.ctx.rotate(Math.random() * 0.005); // random rotation
 		}
+
+		if (this.index < this.dirtyWord.length) {
+			requestAnimationFrame(this.render.bind(this));
+		}
+		this.ctx.clearRect(this.x, this.y, 60, 60);
+		this.ctx.setLineDash([this.dashLen - this.dashOffset, this.dashOffset - this.speed]); // create a long dash mask
+		this.dashOffset -= this.speed; // reduce dash length
+		this.ctx.strokeText(this.dirtyWord[this.index], this.x, this.y + 50); // stroke letter
 	}
 }
